67d3f789e7ecb2deecaacdafe768ce24b80c4f76
rionberi <berisharion722@gmail.com>
2025-03-08 10:53:40 +0100

    dita10

---
+++ add.php
@@ -0,0 +1,42 @@
+<?php
+
+include_once 'config.php';
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $email = $_POST['email'];
+
+    $sql = "INSERT INTO users (name, surname ,email) VALUES (:name, :surname, :email)";
+    $sqlQuery = $conn->prepare($sql);
+
+    $sqlQuery->bindParam(":name",$name);
+    $sqlQuery->bindParam(":surname",$surname);
+    $sqlQuery->bindParam(":email",$email);
+
+    $sqlQuery->execute();
+
+    echo "Data saved successfully! <br>";
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add users</title>
+</head>
+<body>
+    <form action="add.php" method="POST">
+        <input type="text" name="name" placeholder="Name"><br>
+        <input type="text" name="surname" placeholder="Surname"><br>
+        <input type="email" name="email" placeholders="Email"><br>
+        <input type="submit" name="submit">Add</input>
+
+    </form>
+    <a href="dashbord.php">Dashboard</a>
+</body>
+</html>
---
+++ dashbord.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        table,td,th{
+            border:solid 1pc black;
+            border-collapse:collapse;
+        }
+
+        td,th{
+            padding:10px 20px;
+        }
+    </style>
+
+</head>
+<body>
+        <?php
+            include_once 'config.php';
+            $sql = "SELECT * FROM users";
+            $getUsers = $conn->prepare($sql);
+            $getUsers->execute();
+
+            $users = $getUsers->fetchALL();
+        ?>
+<table>
+    <thead>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Surname</th>
+        <th>Email</th>
+    </thead>
+    <tbody>
+        <?php
+        foreach($users as $user) {
+        ?>
+        <tr>
+            <td><?php echo $user['id']?></td>
+            <td><?php echo $user['name']?></td>
+            <td><?php echo $user['surname']?></td>
+            <td><?php echo $user['email']?></td>
+        </tr>
+        
+    <?php
+    }
+
+    ?>
+    </tbody>
+</table>
+<a href="add.php">Add new users</a>
+    
+</body>
+</html>
